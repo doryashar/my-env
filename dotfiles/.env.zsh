9df38d4de7f5983e8942f00409600af477496850
doryashar <dor.yashar@gmail.com>
2025-03-15 21:34:12 +0200

    Auto-sync dotfiles 2025-03-15 21:34:12

--- dotfiles/.env.zsh
+++ dotfiles/.env.zsh
@@ -2,10 +2,13 @@ export ENV_LOC="/home/yashar/env" #
 # "${ENV_LOC:-"$( cd "$( dirname "$( readlink -f "${0}" )" )" && pwd )"}"
 
 # Load config
+
 # If modified time of env is more than  1 day, then update it and dotfiles
-if [ ! -f "${ENV_LOC}/tmp/updated" ] || [ "$(find "${ENV_LOC}/tmp/updated" -mtime +1 -print)" ]; then
-    echo "Updating env..."
-    ${ENV_LOC}/scripts/sync_env.sh --encrypted_sync --dotfiles_sync --push --pull && touch ${ENV_LOC}/tmp/updated
+if [ -t 0 ]; then
+    if [ ! -f "${ENV_LOC}/tmp/updated" ] || [ "$(find "${ENV_LOC}/tmp/updated" -mtime +1 -print)" ]; then
+        echo "Updating env..."
+        ${ENV_LOC}/scripts/sync_env.sh --encrypted_sync --dotfiles_sync --push --pull && touch ${ENV_LOC}/tmp/updated
+    fi
 fi
 
 if [ -f "${ENV_LOC}/private/.secrets" ]; then

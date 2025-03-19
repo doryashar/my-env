80e0a5e70c9d98b7aa0babb8afabc61f2fab12d6
Dor <dor.yashar@gmail.com>
2025-03-19 10:08:11 +0200

    Auto-sync dotfiles 2025-03-19 10:08:11

--- dotfiles/.env.zsh
+++ dotfiles/.env.zsh
@@ -6,7 +6,10 @@ ENV_LOC="${ENV_LOC:-"$( dirname "$( dirname "$( readlink -f "${0}" )" )" )"}"
 . ${ENV_LOC}/env_vars
 
 if [ -f "${ENV_LOC}/private/secrets" ]; then
-. ${ENV_LOC}/private/secrets
+    . ${ENV_LOC}/private/secrets
+    echo "BW session is: $BW_SESSION"
+else
+    echo "Secrets was not found in "${ENV_LOC}/private/secrets""
 fi
 
 . ${ENV_LOC}/functions/*
